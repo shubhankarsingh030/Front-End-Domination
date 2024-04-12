c44e52fddcf3cc16393fe7584ca15e748dfb58a3
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2024-04-12 17:05:57 +0530

    Page1,page2

--- Final Project/script.js
+++ Final Project/script.js
@@ -23,7 +23,7 @@ function loadingAnimation () {
     tl.to("#loader",{
         opacity:0,
         duration:0.2,
-        delay:0,
+        delay:3,
     });
     tl.from("#page1",{
         delay:0.2,
---
+++ Works-Studio/script.js
@@ -0,0 +1,52 @@
+let tl = gsap.timeline();
+let elem = document.querySelectorAll(".elem");
+let page2 = document.querySelector("#page2")
+
+
+function loader() {
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.5,
+        ease: "power1",
+    });
+    tl.from(
+        "#yellow2",
+        {
+            top: "100%",
+            delay: 0.5,
+            duration: 0.5,
+            ease: "power1",
+        },
+        "anim"
+    );
+    tl.to(
+        "#loader h1",
+        {
+            delay: 0.6,
+            duration: 0.5,
+            color: "black",
+        },
+        "anim"
+    );
+    tl.to("#loader", {
+        opacity: 0,
+    });
+    tl.to("#loader", {
+        display: "none",
+    });
+}
+loader();
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+});
+
+elem.forEach(function(ele){
+    ele.addEventListener("mouseenter",function(){
+        let bgimg = ele.getAttribute("data-img");
+        page2.style.backgroundImage = `url(${bgimg})`
+    })
+})
+
